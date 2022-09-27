589ec38b17133b026779a587f6d43621046cef9c
alexjhoan <alexjhoan.25@gmail.com>
2022-09-27 12:49:04 -0400

    add dealercar into cardetails

--- pages/auto/[id].tsx
+++ pages/auto/[id].tsx
@@ -17,6 +17,7 @@ import { CustomThumbsSwiper } from '../../components/CustomSwiper'
 import { grey } from '@mui/material/colors'
 import EmailIcon from '@mui/icons-material/Email'
 import { WhatsappIcon } from '../../components/WhatsappIcon'
+import DealerCars from '../../components/Dealerships'
 
 const StyledVideoResponsive = styled(Box)(({ theme }) => ({
   height: 0,
@@ -62,124 +63,127 @@ const Auto = () => {
     'tracción'
   ]
   return (
-    <Container maxWidth="lg" sx={{ py: 5 }}>
-      <Typography variant="body1" sx={{ fontWeight: 700 }}>
-        Nombre Car
-      </Typography>
-      <Typography variant="h5" sx={{ fontWeight: 700 }}>
-        Desde USD 25.990
-      </Typography>
-      <Grid container spacing={4}>
-        <Grid item xs={12} md={7}>
-          <CustomThumbsSwiper imageList={Array(10).fill(1)} />
-          <Box my={5}>
-            <Typography
-              variant={isMd ? 'h5' : 'h4'}
-              color="secondary"
-              sx={{ fontWeight: 700 }}
-              gutterBottom
-            >
-              Atributos destacados
-            </Typography>
-            <Typography variant="body1">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum dolorem unde enim
-              expedita nulla obcaecati magni dignissimos officia itaque nesciunt, in odit suscipit
-              consequatur aliquid fugiat, quidem molestiae dicta. Quia! Lorem ipsum dolor sit amet
-              consectetur adipisicing elit. Nemo libero consequatur maxime omnis esse dignissimos
-              reprehenderit fugit tempora voluptatem placeat dolorum obcaecati perspiciatis a
-              nostrum, qui optio recusandae corrupti dolore.
-            </Typography>
-          </Box>
-          <Box my={5}>
-            <Typography
-              variant={isMd ? 'h5' : 'h4'}
-              color="secondary"
-              sx={{ fontWeight: 700 }}
-              gutterBottom
-            >
-              Descripción
-            </Typography>
-            <ul style={{ paddingLeft: 16, columns: 3 }}>
-              <li>Lorem, ipsum dolor.</li>
-              <li>Lorem, ipsum dolor.</li>
-              <li>Lorem, ipsum dolor.</li>
-              <li>Lorem, ipsum dolor.</li>
-              <li>Lorem, ipsum dolor.</li>
-              <li>Lorem, ipsum dolor.</li>
-              <li>Lorem, ipsum dolor.</li>
-              <li>Lorem, ipsum dolor.</li>
-              <li>Lorem, ipsum dolor.</li>
-            </ul>
-          </Box>
-          <Box my={5}>
-            <Typography
-              variant={isMd ? 'h5' : 'h4'}
-              color="secondary"
-              sx={{ fontWeight: 700 }}
-              gutterBottom
-            >
-              Características
-            </Typography>
-            {feature.map((item, j) => (
-              <Grid container sx={{ borderBottom: `solid 1px ${grey[500]}` }} key={j} py={1}>
-                <Grid item xs={12} sm={5}>
-                  <Typography
-                    variant="body1"
-                    color="initial"
-                    sx={{ textTransform: 'capitalize', fontWeight: 700 }}
-                  >
-                    {item}
-                  </Typography>
+    <>
+      <Container maxWidth="lg" sx={{ py: 5 }}>
+        <Typography variant="body1" sx={{ fontWeight: 700 }}>
+          Nombre Car
+        </Typography>
+        <Typography variant="h5" sx={{ fontWeight: 700 }}>
+          Desde USD 25.990
+        </Typography>
+        <Grid container spacing={4}>
+          <Grid item xs={12} md={7}>
+            <CustomThumbsSwiper imageList={Array(10).fill(1)} />
+            <Box my={5}>
+              <Typography
+                variant={isMd ? 'h5' : 'h4'}
+                color="secondary"
+                sx={{ fontWeight: 700 }}
+                gutterBottom
+              >
+                Atributos destacados
+              </Typography>
+              <Typography variant="body1">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum dolorem unde enim
+                expedita nulla obcaecati magni dignissimos officia itaque nesciunt, in odit suscipit
+                consequatur aliquid fugiat, quidem molestiae dicta. Quia! Lorem ipsum dolor sit amet
+                consectetur adipisicing elit. Nemo libero consequatur maxime omnis esse dignissimos
+                reprehenderit fugit tempora voluptatem placeat dolorum obcaecati perspiciatis a
+                nostrum, qui optio recusandae corrupti dolore.
+              </Typography>
+            </Box>
+            <Box my={5}>
+              <Typography
+                variant={isMd ? 'h5' : 'h4'}
+                color="secondary"
+                sx={{ fontWeight: 700 }}
+                gutterBottom
+              >
+                Descripción
+              </Typography>
+              <ul style={{ paddingLeft: 16, columns: 3 }}>
+                <li>Lorem, ipsum dolor.</li>
+                <li>Lorem, ipsum dolor.</li>
+                <li>Lorem, ipsum dolor.</li>
+                <li>Lorem, ipsum dolor.</li>
+                <li>Lorem, ipsum dolor.</li>
+                <li>Lorem, ipsum dolor.</li>
+                <li>Lorem, ipsum dolor.</li>
+                <li>Lorem, ipsum dolor.</li>
+                <li>Lorem, ipsum dolor.</li>
+              </ul>
+            </Box>
+            <Box my={5}>
+              <Typography
+                variant={isMd ? 'h5' : 'h4'}
+                color="secondary"
+                sx={{ fontWeight: 700 }}
+                gutterBottom
+              >
+                Características
+              </Typography>
+              {feature.map((item, j) => (
+                <Grid container sx={{ borderBottom: `solid 1px ${grey[500]}` }} key={j} py={1}>
+                  <Grid item xs={12} sm={5}>
+                    <Typography
+                      variant="body1"
+                      color="initial"
+                      sx={{ textTransform: 'capitalize', fontWeight: 700 }}
+                    >
+                      {item}
+                    </Typography>
+                  </Grid>
+                  <Grid item xs={12} sm={7}>
+                    <Typography variant="body1" color="initial">
+                      Lorem ipsum dolor sit amet.
+                    </Typography>
+                  </Grid>
                 </Grid>
-                <Grid item xs={12} sm={7}>
-                  <Typography variant="body1" color="initial">
-                    Lorem ipsum dolor sit amet.
-                  </Typography>
-                </Grid>
-              </Grid>
-            ))}
-          </Box>
-          <StyledVideoResponsive>
-            <iframe
-              width="1280"
-              height="720"
-              src="https://www.youtube.com/embed/EPVGRTA72tU"
-              title="¿Y VOS, PODÉS MANEJAR ASÍ?"
-              frameBorder="0"
-              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-              allowFullScreen
-            ></iframe>
-          </StyledVideoResponsive>
-          <Stack
-            direction={isXs ? 'column' : 'row'}
-            justifyContent={'space-between'}
-            spacing={2}
-            mt={2}
-          >
-            <Button
-              variant="contained"
-              color="primary"
-              fullWidth
-              sx={{ fontWeight: 700, fontSize: 18 }}
+              ))}
+            </Box>
+            <StyledVideoResponsive>
+              <iframe
+                width="1280"
+                height="720"
+                src="https://www.youtube.com/embed/EPVGRTA72tU"
+                title="¿Y VOS, PODÉS MANEJAR ASÍ?"
+                frameBorder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                allowFullScreen
+              ></iframe>
+            </StyledVideoResponsive>
+            <Stack
+              direction={isXs ? 'column' : 'row'}
+              justifyContent={'space-between'}
+              spacing={2}
+              mt={2}
             >
-              Descargar catálogo
-            </Button>
-            <Stack direction={'row'} justifyContent={'center'} spacing={2} alignItems={'center'}>
-              <Typography variant="body2">Compartir</Typography>
-              <StyledBtnShared>
-                <EmailIcon sx={{ color: theme.palette.common.white }} />
-              </StyledBtnShared>
-              <StyledBtnShared>
-                <WhatsappIcon sx={{ color: theme.palette.common.white }} />
-              </StyledBtnShared>
+              <Button
+                variant="contained"
+                color="primary"
+                fullWidth
+                sx={{ fontWeight: 700, fontSize: 18 }}
+              >
+                Descargar catálogo
+              </Button>
+              <Stack direction={'row'} justifyContent={'center'} spacing={2} alignItems={'center'}>
+                <Typography variant="body2">Compartir</Typography>
+                <StyledBtnShared>
+                  <EmailIcon sx={{ color: theme.palette.common.white }} />
+                </StyledBtnShared>
+                <StyledBtnShared>
+                  <WhatsappIcon sx={{ color: theme.palette.common.white }} />
+                </StyledBtnShared>
+              </Stack>
             </Stack>
-          </Stack>
+          </Grid>
+          <Grid item xs={12} md={5}>
+            <CalculationLoan />
+          </Grid>
         </Grid>
-        <Grid item xs={12} md={5}>
-          <CalculationLoan />
-        </Grid>
-      </Grid>
-    </Container>
+      </Container>
+      <DealerCars />
+    </>
   )
 }
 
